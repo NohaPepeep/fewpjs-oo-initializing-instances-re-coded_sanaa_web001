5d74d6a9042378c05a8f9451d577aac22b8c7316
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-12 05:17:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ class Lunch{
         this.drink=drink;
   }
 }
+class Lunch{
+  constructor(salad, soup,drink){
+        this.salad=salad;
+        this.soup=soup;
+        this.drink=drink;
+  }
+}
